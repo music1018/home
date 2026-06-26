527f97aaac42f940b3e7618be83bfc379cbde4cc
iwasaka riku <riku.tennis.1018@gmail.com>
2026-06-27 00:26:29 +0900

    Rename author from Kaito to r and enforce custom SVG favicon globally

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kaito';
-export const SITE_DESCRIPTION = 'Kaito Portfolio & Blog';
+export const SITE_TITLE = 'r';
+export const SITE_DESCRIPTION = 'r Portfolio & Blog';
